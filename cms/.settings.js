cc00a350f140c6a998cee2955e9e2a29d310e70a
Martin Gottweis <gottwik@gmail.com>
2016-08-11 15:27:30 +0200

    added demo user

--- cms/.settings.js
+++ cms/.settings.js
@@ -1,6 +1,8 @@
 {
 	settings: {
 		$admin_background_image_type: 'image',
-		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.demo/direct_uploads/1470906095_pexels-photo-27484.jpg'
+		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.demo/direct_uploads/1470906095_pexels-photo-27484.jpg',
+		dark: true,
+		login_message: 'Use username: demo, password: demo',
 	}
 }
--- cms/.users.js
+++ cms/.users.js
@@ -6,5 +6,14 @@
 			hash: '0ebaee25856653053da3e2ea4f989d4aed29c32b5dcdfc264d34cc72ef7f62a3',
 			user_created_timestamp: 1465969880306
 		},
+		{
+			username: 'demo',
+			tags: [
+				'demo'
+			],
+			salt: '926425b97bdb955885f9ad45a682490a',
+			hash: '8bcde31f1b5e9033862f44f0c647da73d1bc0edcd748b68d69356506e018f312',
+			user_created_timestamp: 1470922016343
+		}
 	]
 }
