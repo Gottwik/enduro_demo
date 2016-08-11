f495c64a7adc36c4615f178b8e780cb46d67170d
Martin Gottweis <gottwik@gmail.com>
2016-08-11 16:49:13 +0200

    added secure to gitignore

--- cms/.settings.js
+++ cms/.settings.js
@@ -1,8 +1,8 @@
 {
 	settings: {
 		$admin_background_image_type: 'image',
-		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.demo/direct_uploads/1470906095_pexels-photo-27484.jpg',
+		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.demo/direct_uploads/1470922572_landscape-mountains-nature-clouds.jpg',
 		dark: true,
-		login_message: 'Use username: demo, password: demo',
+		login_message: 'Use username: demo, password: demo'
 	}
 }
