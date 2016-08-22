09266488c6f32663e481e7cd147d4290f5ad53ae
Martin Gottweis <gottwik@gmail.com>
2016-08-22 16:41:36 +0200

    added favicon

--- cms/.settings.js
+++ cms/.settings.js
@@ -1,7 +1,7 @@
 {
 	settings: {
 		$admin_background_image_type: 'image',
-		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.demo/direct_uploads/1470922572_landscape-mountains-nature-clouds.jpg',
+		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.demo/direct_uploads/1471875396_dinner-meal-table-wine.jpg',
 		dark: true,
 		login_message: 'Use username: demo, password: demo'
 	}
