da6515302632cd29fcd3d54309bfbc606d7dea2f
Martin Gottweis <gottwik@gmail.com>
2016-08-31 11:49:55 +0200

    bugfixed bootstrap_selection

--- cms/.settings.js
+++ cms/.settings.js
@@ -1,7 +1,7 @@
 {
 	settings: {
 		$admin_background_image_type: 'image',
-		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.demo/direct_uploads/1471875396_dinner-meal-table-wine.jpg',
+		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.demo/direct_uploads/1472632823_732oj9ai8xlghhyqfr.png',
 		dark: true,
 		login_message: 'Use username: demo, password: demo'
 	}
